60baa817c8443000e8e859e63a3aedd1cbdde46c
ronish11 <ronishsharma9@gmail.com>
2020-05-02 06:57:12 +0545

    comment

--- app/src/main/java/com/example/ui_desigin/MainActivity.java
+++ app/src/main/java/com/example/ui_desigin/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public String name,pass;
     public Button login,createnew;
 
+    // just for check
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
