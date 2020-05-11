af4e9dd0b98261728199cd78ae3349004f757894
ronish11 <ronishsharma9@gmail.com>
2020-05-11 16:38:30 +0545

    LIVE_DATA

--- app/src/main/java/com/example/ui_desigin/TODO.java
+++ app/src/main/java/com/example/ui_desigin/TODO.java
@@ -1,19 +1,25 @@
 package com.example.ui_desigin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 
+import com.example.ui_desigin.ViewModel.ListActivityViewModel;
 import com.example.ui_desigin.database.NoteEntity;
 import com.example.ui_desigin.Model.NotesAdepter;
-import com.example.ui_desigin.utils.SampleDataProvider;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -22,7 +28,10 @@ import butterknife.OnClick;
 
 public class TODO extends AppCompatActivity {
 
-    private List<NoteEntity> mNoteList;
+
+    private List<NoteEntity> mNotesList = new ArrayList<>();
+    private ListActivityViewModel mViewModel;
+    NotesAdepter mNotesAdepter;
 
     @BindView(R.id.notes_recyclerview)
     RecyclerView mRecyclerview;
@@ -40,24 +49,38 @@ public class TODO extends AppCompatActivity {
         Toolbar toolbar= findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initViewModel();
+
+
         ButterKnife.bind(this);
         initRecyclerView();
 
-        mNoteList= SampleDataProvider.getSampleData();
-        showData();
     }
 
-    private Void setSupportActionBar(Toolbar toolbar) {
+    private void initViewModel() {
+
+        Observer<List<NoteEntity>> notesObserver=
+                noteEntities -> {
+
+                    mNotesList.clear();
+                    assert noteEntities != null;
+                    mNotesList.addAll(noteEntities);
+
+                    if(mNotesAdepter == null)
+                    {
+                        mNotesAdepter = new NotesAdepter(TODO.this,mNotesList);
+                        mRecyclerview.setAdapter(mNotesAdepter);
+                    }else{
+                        mNotesAdepter.notifyDataSetChanged();
+                    }
+                };
+        mViewModel= ViewModelProviders.of(this).get(ListActivityViewModel.class);
+        mViewModel.mNotesList.observe(TODO.this,notesObserver);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
         Void o = null;
-        return o;
     }
-
-    private void showData()
-    {
-        NotesAdepter notesAdepter= new NotesAdepter(this,mNoteList);
-        mRecyclerview.setAdapter(notesAdepter);
-    }
-
 
     private void initRecyclerView() {
 
@@ -65,6 +88,29 @@ public class TODO extends AppCompatActivity {
         LinearLayoutManager layoutManager= new LinearLayoutManager(this);
         mRecyclerview.setLayoutManager(layoutManager);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+
+        switch(id){
+            case R.id.add_sample_data:{
+                addSampleData();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addSampleData() {
+        mViewModel.addSampleData();
     }
 }
 
