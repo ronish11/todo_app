c64d8864ffef52ee951f68101b0321f4e77516c4
ronish11 <ronishsharma9@gmail.com>
2020-05-12 09:46:33 +0545

    INSERT DATA

--- app/src/main/java/com/example/ui_desigin/TODO.java
+++ app/src/main/java/com/example/ui_desigin/TODO.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,6 +12,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 
@@ -87,6 +89,27 @@ public class TODO extends AppCompatActivity {
         mRecyclerview.hasFixedSize();
         LinearLayoutManager layoutManager= new LinearLayoutManager(this);
         mRecyclerview.setLayoutManager(layoutManager);
+
+        ItemTouchHelper itemTouchHelper= new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+                deleteNote(mNotesAdepter.getNotePosition(viewHolder.getAdapterPosition()));
+            }
+        });
+
+        itemTouchHelper.attachToRecyclerView(mRecyclerview);
+
+    }
+
+    private void deleteNote(NoteEntity noteEntity) {
+        mViewModel.deleteNote(noteEntity);
+        Toast.makeText(this, "Note Sucessfully Deleted", Toast.LENGTH_SHORT).show();
 
     }
 
